ed68d747f0508984fcf68eeb3259a4f3e2dc580c
ahmad <ahmad.shakla>
2020-06-16 17:23:44 +0300

    hbd

---
+++ app/src/main/java/com/example/babyimhome/MyApp.java
@@ -0,0 +1,15 @@
+//package com.example.babyimhome;
+//
+//import android.app.Application;
+//import android.content.BroadcastReceiver;
+//import android.content.IntentFilter;
+//
+//public class MyApp extends Application {
+//    private final static String ACTION = "POST_PC.ACTION_SEND_SMS";
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        BroadcastReceiver broadcastReceiver = new LocalSendSmsBroadcastReceiver();
+//        registerReceiver(broadcastReceiver, new IntentFilter(ACTION));
+//    }
+//}
